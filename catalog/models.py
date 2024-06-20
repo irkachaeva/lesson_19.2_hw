423bacd3af4ed73e8ce8a0d6e98d29eb887db11c
ira_kachaeva <irenkachaeva@gmail.com>
2024-06-20 10:30:23 +0300

    Подключили СУБД PostgreSQL для работы: создали базу данных в ручном режиме. внесли изменения в настройки подключения. в каталоге сделали модели: Товары, Категории

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование', help_text='Наименование товара')
+    description = models.CharField(max_length=250, verbose_name='Описание товара')
+    image = models.ImageField(upload_to='catalog/images', verbose_name='Изображение(превью)', blank=True, null=True)
+    category_name = models.CharField(max_length=50, verbose_name='Категория')
+    price = models.IntegerField(verbose_name='Цена за покупку')
+    data_creation = models.DateTimeField(verbose_name='Дата создания(записи в БД)')
+    data_last_change = models.DateTimeField(verbose_name='Дата последнего изменения(записи в БД)')
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+        ordering = ['category_name', 'price', 'data_creation', 'data_last_change']
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=50, verbose_name='Наименование категории')
+    description = models.CharField(max_length=250, verbose_name='Описание категории')
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['category_name']
+
+    def __str__(self):
+        return self.category_name
+
