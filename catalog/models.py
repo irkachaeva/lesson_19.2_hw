8ad8ada3371d4008583acfb4910a7f24e8777832
ikachaeva <irenkachaeva@gmail.com>
2024-06-27 00:12:31 +0300

    Задание 4

--- catalog/models.py
+++ catalog/models.py
@@ -1,33 +1,36 @@
 from django.db import models
 
 
-class Product(models.Model):
-    name = models.CharField(max_length=100, verbose_name='Наименование', help_text='Наименование товара')
-    description = models.CharField(max_length=250, verbose_name='Описание товара')
-    image = models.ImageField(upload_to='catalog/images', verbose_name='Изображение(превью)', blank=True, null=True)
-    category_name = models.CharField(max_length=50, verbose_name='Категория')
-    price = models.IntegerField(verbose_name='Цена за покупку')
-    data_creation = models.DateTimeField(verbose_name='Дата создания(записи в БД)')
-    data_last_change = models.DateTimeField(verbose_name='Дата последнего изменения(записи в БД)')
-
-    class Meta:
-        verbose_name = 'Товар'
-        verbose_name_plural = 'Товары'
-        ordering = ['category_name', 'price', 'data_creation', 'data_last_change']
-
-    def __str__(self):
-        return self.name
-
-
 class Category(models.Model):
-    category_name = models.CharField(max_length=50, verbose_name='Наименование категории')
+    category = models.CharField(max_length=50, verbose_name='Наименование категории')
     description = models.CharField(max_length=250, verbose_name='Описание категории')
 
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
-        ordering = ['category_name']
+        ordering = ['category']
 
     def __str__(self):
-        return self.category_name
+        return self.category
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, verbose_name='Категория')
+    name = models.CharField(max_length=100, verbose_name='Наименование', help_text='Наименование товара')
+    description = models.TextField(max_length=250, verbose_name='Описание товара', blank=True, null=True)
+    image = models.ImageField(upload_to='catalog/images', verbose_name='Изображение(превью)', blank=True, null=True)
+    price = models.IntegerField(verbose_name='Цена за покупку')
+    created_at = models.DateTimeField(verbose_name='Дата создания(записи в БД)')
+    updated_at = models.DateTimeField(verbose_name='Дата последнего изменения(записи в БД)')
+    manufactured_at = models.DateTimeField(verbose_name='Дата производства продукта', blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+        ordering = ['category', 'price', 'created_at', 'updated_at', 'manufactured_at']
+
+    def __str__(self):
+        return self.name
+
+
 
