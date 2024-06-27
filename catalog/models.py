47d078e676d07f20efefa55f2c3fdb3a208b2ef7
ikachaeva <irenkachaeva@gmail.com>
2024-06-27 23:40:57 +0300

    postgres зависимость добавлена поле с именем - название изменено поле цены - формат исправлен auto_now и auto_now_add - добавлены MEDIA_ROOT и MEDIA_URL подключено ко всему проекту (config/urls.py)

--- catalog/models.py
+++ catalog/models.py
@@ -15,19 +15,19 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.PROTECT, verbose_name='Категория')
+    title = models.ForeignKey(Category, on_delete=models.PROTECT, verbose_name='Категория')
     name = models.CharField(max_length=100, verbose_name='Наименование', help_text='Наименование товара')
     description = models.TextField(max_length=250, verbose_name='Описание товара', blank=True, null=True)
     image = models.ImageField(upload_to='catalog/images', verbose_name='Изображение(превью)', blank=True, null=True)
-    price = models.IntegerField(verbose_name='Цена за покупку')
-    created_at = models.DateTimeField(verbose_name='Дата создания(записи в БД)')
-    updated_at = models.DateTimeField(verbose_name='Дата последнего изменения(записи в БД)')
+    price = models.PositiveIntegerField(verbose_name='Цена за покупку')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания(записи в БД)')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения(записи в БД)')
     manufactured_at = models.DateTimeField(verbose_name='Дата производства продукта', blank=True, null=True)
 
     class Meta:
         verbose_name = 'Товар'
         verbose_name_plural = 'Товары'
-        ordering = ['category', 'price', 'created_at', 'updated_at', 'manufactured_at']
+        ordering = ['title', 'price', 'created_at', 'updated_at', 'manufactured_at']
 
     def __str__(self):
         return self.name
--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,5 +128,8 @@ STATICFILES_DIRS = (BASE_DIR/'static',)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/images/'
-MEDIA_ROOT: Path = BASE_DIR /'images'
+# MEDIA_URL = '/images/'
+# MEDIA_ROOT: Path = os.path.join(BASE_DIR /'images'
+
+MEDIA_URL = "/images/"
+MEDIA_ROOT = os.path.join(BASE_DIR, MEDIA_URL[1:])
