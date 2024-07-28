5abf5c12b8654af27b5a56051932c03a79b3be2b
ikachaeva <irenkachaeva@gmail.com>
2024-07-28 22:12:02 +0300

    20.2 шаблонный фильтр - правки: cnhfybwf product_detail.html

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,8 +6,7 @@ from catalog.views import product_detail
 app_name = AppConfig.__name__
 
 urlpatterns = [
-    path('', product_list, name='base'),
-    #path('products/', product_list, name='product_list'),
+    path('', product_list, name='product_list'),
     path('catalog/<int:pk>', product_detail, name='product_detail'),
     path('contacts/', contacts, name='contacts')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -5,17 +5,16 @@ from catalog.models import Product
 def product_list(request):
     products = Product.objects.all()
     context = {"products": products}
-    return render(request, 'product_detail.html', context)
+    return render(request, 'product_list.html', context)
 
 
 def contacts(request):
     return render(request, 'contacts.html')
 
 
-
-
 def product_detail(request, pk):
-    context = {'object': get_object_or_404(Product, pk=pk)}
+    product = get_object_or_404(Product.objects.get(pk=pk))
+    context = {'product': product}
     return render(request, 'product_detail.html', context)
 
 
