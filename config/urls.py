61b2fc4fde8a12f85b5545f5da3a21f1d45e941d
ikachaeva <irenkachaeva@gmail.com>
2024-08-02 21:42:39 +0300

    Задание 2 21.1 правки

--- config/urls.py
+++ config/urls.py
@@ -24,7 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls', namespace='catalog')),
     path('blog/', include('blog.urls', namespace='blog')),
-    path('blog/admin/', admin.site.urls),
     ]
 
 if settings.DEBUG:
