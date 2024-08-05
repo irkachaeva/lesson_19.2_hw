fa59c965766ed2807faa3e40b7bb39cc9f8c7fdf
ikachaeva <irenkachaeva@gmail.com>
2024-08-06 00:53:46 +0300

    Задание 1 22.1 создание и редактирование продукта

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from catalog.models import Product
 
+stop_text = "казино, криптовалюта, крипта, биржа, дешево, бесплатно, обман, полиция, радар"
+stop_words = set(stop_text.split(", "))
+
 
 class ProductForm(forms.ModelForm):
     class Meta:
@@ -13,4 +16,21 @@ class ProductForm(forms.ModelForm):
         #exclude = 'created_at', 'updated_at'
         #fields = '__all__'
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
 
+    def clean_name(self):
+        cleaned_data = self.cleaned_data.get('name')
+        words = set(cleaned_data.lower().split())
+        if words.intersection(stop_words):
+            raise forms.ValidationError('Ошибка, связанная с названием продукта')
+        return cleaned_data
+
+    def clean_description(self):
+        cleaned_data = self.cleaned_data.get('description')
+        words = set(cleaned_data.lower().split())
+        if words.intersection(stop_words):
+            raise forms.ValidationError('Ошибка, связанная с описанием продукта')
+        return cleaned_data
